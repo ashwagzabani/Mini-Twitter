b8dde433c0990bc31fab44929d746a131355b8ba
ashwagzabani <ashwag.amz@gmail.com>
2021-01-08 22:53:35 +0300

    install react-router-dom and import it in App.js

--- mini-twitter/src/App.js
+++ mini-twitter/src/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 class App extends Component {
   render(){
