fed7ccf7faf64b3289fc583c32accb69a2f1150d
ashwagzabani <ashwag.amz@gmail.com>
2021-01-16 16:16:18 +0300

    add default img to null img

--- mini-twitter/src/components/AxiosContentRow.js
+++ mini-twitter/src/components/AxiosContentRow.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import '../comonents-style/AxiosContentRow.css'
-
+import news from '../news.png'
 class AxiosContentRow extends Component {
     render() {
         return (
             <div className="AxiosContentRow">
-                {this.props.imgUrl !== null ? <img src={this.props.imgUrl} /> : ''}
+                {this.props.imgUrl !== null ? <img src={this.props.imgUrl} /> : <img src={news} />}
                 <p>{this.props.title}</p>
                 {/* <p>{" ` " + this.props.quetContent + " ` "}</p> */}
             </div>
