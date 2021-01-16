6ee206c6ca4c0f7bb38d3ab6e7cf106f6c9c1490
ashwagzabani <ashwag.amz@gmail.com>
2021-01-16 17:32:19 +0300

    edit route

--- mini-twitter/src/App.js
+++ mini-twitter/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
 import {
   HashRouter as Router,
   Switch,
