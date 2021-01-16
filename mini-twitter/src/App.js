45e15b83a45cb164c413a4bd0c2db6bf15db533f
ashwagzabani <ashwag.amz@gmail.com>
2021-01-16 17:40:49 +0300

    edit route

--- mini-twitter/src/App.js
+++ mini-twitter/src/App.js
@@ -55,7 +55,7 @@ class App extends Component {
   render() {
 
     return (
-      <Router basename="/roject-2/">
+      <Router basename="/ashwagzabani/Project-2/">
         <div className="App">
 
           <Route exact path="/" component={LandingPage} />
